db7e6731c795b25e1f6e4c5d7f1738346069f674
N <noelmcloughlin@gmail.com>
2018-06-11 21:44:40 +0100

    Fix unreachable code, jessie, & update kitchen test

--- test/integration/version-1.13.1/testinfra/test_docker.py
+++ test/integration/version-1.13.1/testinfra/test_docker.py
@@ -1,5 +1,10 @@
 import testinfra
 
+def test_package_is_installed(Package):
+    docker = Package('docker-engine')
+    assert docker.is_installed
+    assert docker.version.startswith('1.13.1')
+
 def test_service_is_running_and_enabled(Service):
     docker = Service('docker')
     assert docker.is_running
--- test/integration/version-1.6.2/testinfra/test_docker.py
+++ test/integration/version-1.6.2/testinfra/test_docker.py
@@ -1,10 +1,9 @@
 import testinfra
 
-
 def test_package_is_installed(Package):
-    docker = Package('docker-engine')
+    docker = Package('docker.io')
     assert docker.is_installed
-    assert docker.version.startswith('1.12.2')
+    assert docker.version.startswith('1.6.2')
 
 def test_service_is_running_and_enabled(Service):
     docker = Service('docker')
