451d76d1dd21b87a8fdd1b54a1655ad588e509e2
Imran Iqbal <iqbalmy@hotmail.com>
2019-10-16 23:35:33 +0100

    test(inspec): add tests for package, config & service

---
+++ test/integration/default/controls/service.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+control 'Docker service' do
+  title 'should be running and enabled'
+
+  describe service('docker') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
